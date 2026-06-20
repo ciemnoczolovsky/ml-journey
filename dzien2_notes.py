52feff1a8eb2b04cb930923b6519eacd6e400a11
ciemnoczolovsky <ciemnoczolowski.patryk@gmail.com>
2026-06-20 11:38:26 +0200

    Add day 2 python basics: variables, types, f-strings

---
+++ dzien2_notes.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+print(f"Hello, {name.title()}.\nIt's day 2. You are at the beginning of beatufiul \"RoadToMachineLearning\" journey!\n")
+
+goal = int(input("How many months do you think you will be ready for the job? "))
+weeks = goal * 4
+print(f"{goal} months is about {weeks} weeks. Ambitious, but doable. Fingers crossed!")
+
+lessons_learned = input("What have you learned so far? ")
+print(f"{lessons_learned} in just 1 day? Nice!")
