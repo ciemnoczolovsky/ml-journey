81e61bdb80bfbbdc92ae9d6f789f578d1542c19b
ciemnoczolovsky <ciemnoczolowski.patryk@gmail.com>
2026-07-06 13:58:28 +0200

    Add modules file to keep functions externally

---
+++ football_modules.py
@@ -0,0 +1,5 @@
+def nominated_players(*players):
+    """Printing players nominated to Balon d'Or"""
+    print('\nFollowing players have been nominated to the Balon d\'or:')
+    for player in players:
+        print(f"\t{player}")
