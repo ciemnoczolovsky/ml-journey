e8568d6f9a6f34ec12d3de21fa7b982c53cc2715
ciemnoczolovsky <ciemnoczolowski.patryk@gmail.com>
2026-07-06 13:59:03 +0200

    Add Chapter 8 excercises: modules,imports

---
+++ day9_modules_import.py
@@ -0,0 +1,7 @@
+#import football_modules // general import
+#from football_modules import nominated_players // import of function
+from football_modules import nominated_players as np
+#alias for function ^ alias for whole module: import function as smth
+#import of all functions: from football_modules import *
+
+np('Michael Olise','Harry Kane','Vini Jr')
