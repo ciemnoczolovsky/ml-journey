469951372b8424c8181ec6e2e6fe964eec382121
ciemnoczolovsky <ciemnoczolowski.patryk@gmail.com>
2026-07-09 18:04:50 +0200

    Add Chapter8 and Claude's excercises

--- day9_modules_import.py
+++ day9_modules_import.py
@@ -1,7 +1,45 @@
 #import football_modules // general import
 #from football_modules import nominated_players // import of function
-from football_modules import nominated_players as np
+from football_modules import *
 #alias for function ^ alias for whole module: import function as smth
 #import of all functions: from football_modules import *
 
-np('Michael Olise','Harry Kane','Vini Jr')
+#Claude excercise 1
+match_points = calculate_match_points(3,2)
+print(match_points)
+
+match_points = calculate_match_points(0,2)
+print(match_points)
+
+match_points = calculate_match_points(2,2)
+print(match_points)
+
+#Claude excercise 2
+matches = [
+    {
+        'home_team': 'Tottenham',
+        'away_team': 'Arsenal',
+        'home_goals': 3,
+        'away_goals': 1
+    },
+    {
+        'home_team': 'Newcastle',
+        'away_team': 'Tottenham',
+        'home_goals': 1,
+        'away_goals': 2
+    },
+    {
+        'home_team': 'Tottenham',
+        'away_team': 'Leeds',
+        'home_goals': 4,
+        'away_goals': 1
+    },
+    {
+        'home_team': 'Chelsea',
+        'away_team': 'Tottenham',
+        'home_goals': 3,
+        'away_goals': 4
+    },
+]
+team_stats = build_team_stats("Tottenham",matches)
+print(team_stats)
